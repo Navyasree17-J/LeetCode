1d9ddef0db4a80020cf546c300c14eed1545b0fb
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2025-04-01 21:24:42 +0530

    Time: 5 ms (96.96%), Space: 108.7 MB (44.29%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp=new long[n+1];
+        for(int i=n-1; i>=0; i--){
+            int points=questions[i][0];
+            int brainpower = questions[i][1];
+            long skip = dp[i+1];
+            long solve = points + (i + brainpower + 1<n? dp[i+brainpower + 1] : 0);
+            dp[i]=Math.max(skip, solve);
+        }
+        return dp[0];  
+    }
+}
