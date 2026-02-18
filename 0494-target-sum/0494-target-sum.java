38ba23696cc5e916dd1720a206be9cdde203d229
Navyasree17-J <jagarlamudi.navyasree@gmail.com>
2026-02-18 10:19:10 +0530

    Time: 2 ms (99.69%), Space: 43.1 MB (77.7%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sm = 0;
+        for(int num:nums) sm += num;
+        if(sm < Math.abs(target) || (sm + target) % 2 != 0) return 0;
+        int totalsum = (sm + target) / 2;
+        int[] dp = new int[totalsum + 1];
+        dp[0] = 1;
+        for(int num : nums){
+            for(int T = totalsum; T >= num;T--){
+                dp[T] += dp[T - num];
+            }
+        }
+        return dp[totalsum];
+    }
+}
